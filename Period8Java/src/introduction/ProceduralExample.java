a9bb1769c5835a2b3a31733fa2fb307b770debd3
max <max@192.168.0.10>
2016-09-15 18:21:20 -0400

    Added String arrays and for loop (courtesy Simon Kahn)

--- Period8Java/src/introduction/ProceduralExample.java
+++ Period8Java/src/introduction/ProceduralExample.java
@@ -4,5 +4,13 @@ public class ProceduralExample {
 	public static void main(String[] args){
 		System.out.println("Hello again World!");
 		System.out.println("Back in business boiiii!");
+
+		String[] names={"Jillian", "Joseph", "Jordan"}; 
+		String[] years={"Senior", "Junior", "Freshmen"}; 
+		//System.out.println("hi, my name is" +names[0]+".");
+
+		for(int i=1;i<=3;i++){
+			System.out.println("hi, my name is " +names[i-1]+". I am a " + years[i-1] + ".");
+		}
 	}
 }
