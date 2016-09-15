aa343eb782fe6bccc769db264f5348b33d93adc6
max <max@192.168.0.10>
2016-09-15 17:17:10 -0400

    got it working at home, i think

--- Period8Java/src/introduction/ProceduralExample.java
+++ Period8Java/src/introduction/ProceduralExample.java
@@ -3,5 +3,6 @@ package introduction;
 public class ProceduralExample {
 	public static void main(String[] args){
 		System.out.println("Hello again World!");
+		System.out.println("Back in business boiiii!");
 	}
 }
