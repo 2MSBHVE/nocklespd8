6fa42bf3adfd1139ebb33f8cec25489c0d8d87be
Tech Squad <Tech Squad@10.8.33.104>
2016-09-16 13:09:41 -0400

    Three students

--- Period8Java/src/introduction/OOPExample.java
+++ Period8Java/src/introduction/OOPExample.java
@@ -4,7 +4,11 @@ public class OOPExample {
 
 	public static void main(String[] args) {
 		Student jillian = new Student("Jillian");
+		Student joseph = new Student("Joseph");
+		Student jordan = new Student("Jordan");
 		jillian.talk();
+		jordan.talk();
+		joseph.talk();
 	}
 
 }
