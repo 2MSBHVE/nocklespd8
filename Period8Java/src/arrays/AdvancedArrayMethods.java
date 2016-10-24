c51b6756eed3d5b728fbb0a472994ff9810ada48
2MSBHVE <max@miku.nyc>
2016-10-24 12:54:46 -0400

    stuffu

--- Period8Java/src/arrays/AdvancedArrayMethods.java
+++ Period8Java/src/arrays/AdvancedArrayMethods.java
@@ -1,38 +1,54 @@
 package arrays;
 
 public class AdvancedArrayMethods {
+
 	
-	static int[] array;
 	
 	public static void main(String[] args) {
-		array = new int[100];
-		int[] someArray = new int[50];
-		methodA(someArray);
+		String[] array = {"a","b","c","d","e","f","g","h"};
+		swap(array, 0,array.length -1);
 		
+		
+	}
+
+	private static void swap(String[] arr, int a, int b) {
+		String placeholder = arr[b];
+		arr[b] = arr[a];
+		arr[a] = placeholder;
 	}
 
 	private static void methodA(int[] someArray) {
 		int[] newArray = new int[someArray.length];
 		
-		
 	}
 	
-	public static void copyArray(int[] original, int[] target){
+	public static void copyArray(int[] original, 
+			int[] target){
 		if(original.length == target.length){
-			for (int i = 0; i < original.length; i++) {
+			for(int i = 0; i < original.length; i++){
 				target[i] = original[i];
 			}
-		}
-		else {
-//			print an "error" message
-			System.out.println("ERROR: tried to copy arrays of two different lengths.");
+		}else{
+			//print an "error" message
+			System.out.println("ERROR: tried to copy arrays"
+					+ " of two different lengths.");
 		}
 	}
-	
-	public static void swap(String arr[], int a, int b){
-		String placeholder = arr[b];
-		arr[b] = arr[a];
-		arr[a] = placeholder;
-	}
-	
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
