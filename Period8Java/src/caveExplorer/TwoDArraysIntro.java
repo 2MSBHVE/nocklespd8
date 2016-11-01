f7ae46d7ba04c2d71aa22dee822f25f8fc254cd4
2MSBHVE <max@miku.nyc>
2016-11-01 13:55:18 -0400

    inclass backup push

--- Period8Java/src/caveExplorer/TwoDArraysIntro.java
+++ Period8Java/src/caveExplorer/TwoDArraysIntro.java
@@ -1,6 +1,7 @@
 package caveExplorer;
 
 import java.util.Arrays;
+import java.util.Iterator;
 
 public class TwoDArraysIntro {
 
@@ -8,19 +9,34 @@ public class TwoDArraysIntro {
 //		String[] xox = {"x","o","x","o","x"};
 //		System.out.println(Arrays.toString(xox));
 	
-		String[][] arr2D = new String[5][4];
+//		String[][] arr2D = new String[5][4];
 	
-		for (int row = 0; row < arr2D.length; row++) {
-			for (int col = 0; col < arr2D[row].length; col++) {
-				arr2D[row][col] = "(" + row + ", " + col + ")";
+//		for (int row = 0; row < arr2D.length; row++) {
+//			for (int col = 0; col < arr2D[row].length; col++) {
+//				arr2D[row][col] = "(" + row + ", " + col + ")";
+//			}
+//		}
+		
+////		every element in a 2D array is itself an array
+//		for (String[] row : arr2D) {
+//			System.out.println(Arrays.toString(row));
+//		}
+		
+		
+		String[][] pic = new String[8][8];
+		
+		for (int row = 0; row < pic.length; row++) {
+			for (int col = 0; col < pic[row].length; col++) {
+				pic[row][col] = " ";
 			}
 		}
 		
-		
-//		every element in a 2D array is itself an array
-		for (String[] row : arr2D) {
-			System.out.println(Arrays.toString(row));
+		for (String[] row : pic) {
+			for (String col : row){
+				System.out.print(col);
+			}
 		}
+		
 	}
 	
 }
