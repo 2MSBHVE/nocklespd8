74949a6c86a964eb63e8332162adc44052af4b31
2MSBHVE <max@miku.nyc>
2016-11-14 13:57:16 -0500

    attempts at coloring console output. failed

--- Period8Java/src/introduction/ProceduralExample.java
+++ Period8Java/src/introduction/ProceduralExample.java
@@ -2,15 +2,19 @@ package introduction;
 
 public class ProceduralExample {
 	public static void main(String[] args){
-		System.out.println("Hello again World!");
-		System.out.println("Back in business boiiii!");
-
-		String[] names={"Jillian", "Joseph", "Jordan"}; 
-		String[] years={"Senior", "Junior", "Freshmen"}; 
-//		System.out.println("hi, my name is" +names[0]+".");
-
-		for(int i=1;i<=3;i++){
-			System.out.println("hi, my name is " +names[i-1]+". I am a " + years[i-1] + ".");
-		}
+//		System.out.println("Hello again World!");
+//		System.out.println("Back in business boiiii!");
+//
+//		String[] names={"Jillian", "Joseph", "Jordan"}; 
+//		String[] years={"Senior", "Junior", "Freshmen"}; 
+////		System.out.println("hi, my name is" +names[0]+".");
+//
+//		for(int i=1;i<=3;i++){
+//			System.out.println("hi, my name is " +names[i-1]+". I am a " + years[i-1] + ".");
+//		}
+		
+		 System.out.println("\033[31;1mHello\033[0m, \033[32;1;2mworld!\033[0m");
+		    System.out.println("\033[31mRed\033[32m, Green\033[33m, Yellow\033[34m, Blue\033[0m");
+		    System.out.println ((char)27 + "[2J");
 	}
 }
