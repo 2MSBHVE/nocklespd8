87a9ec9be51f6a3479c9db8af700efe17c6b784b
2MSBHVE <max@miku.nyc>
2016-11-18 12:29:53 -0500

    stuffu

--- Period8Java/src/introduction/ProceduralExample.java
+++ Period8Java/src/introduction/ProceduralExample.java
@@ -13,8 +13,13 @@ public class ProceduralExample {
 //			System.out.println("hi, my name is " +names[i-1]+". I am a " + years[i-1] + ".");
 //		}
 		
-		 System.out.println("\033[31;1mHello\033[0m, \033[32;1;2mworld!\033[0m");
-		    System.out.println("\033[31mRed\033[32m, Green\033[33m, Yellow\033[34m, Blue\033[0m");
-		    System.out.println ((char)27 + "[2J");
+		 System.out.println("\033[0m BLACK");
+	        System.out.println("\033[31m RED");
+	        System.out.println("\033[32m GREEN");
+	        System.out.println("\033[33m YELLOW");
+	        System.out.println("\033[34m BLUE");
+	        System.out.println("\033[35m MAGENTA");
+	        System.out.println("\033[36m CYAN");
+	        System.out.println("\033[37m WHITE");
 	}
 }
