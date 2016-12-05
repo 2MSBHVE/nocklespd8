a96a3d3236f6187120405e1d5799ffd70fd88651
2MSBHVE <max@miku.nyc>
2016-12-05 14:02:06 -0500

    sorts

--- Period8Java/src/sortingAlgs/sorts.java
+++ Period8Java/src/sortingAlgs/sorts.java
@@ -1,10 +1,15 @@
 package sortingAlgs;
 
-public class InsertionSort {
+import java.util.Arrays;
+
+public class sorts {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+//		bubbleSort(populateRandInts(0, 50, 25));
+		selectionSort(populateRandInts(0, 50, 25));
+		
 	}
 	
 	public static int[] insertionSort(int[] arrIn) {
@@ -49,6 +54,44 @@ public class InsertionSort {
 		
 		return array;
 		
+	}
+	
+	public static void swap(int[] arr, int a, int b) {
+		int temp = arr[a];
+		arr[a] = arr[b];
+		arr[b] = temp;
+	}
+	
+	
+	
+	public static void selectionSort(int[] array){
+		System.out.println("Selection sort with "+Arrays.toString(array));
+		for (int i = 0; i < array.length - 1; i++){
+			int tempLowIndex = i;
+			for (int j = i + 1; j < array.length; j++){
+				if (array[j] < array[tempLowIndex]){
+					tempLowIndex = j;
+				}
+			}
+			if(tempLowIndex!=i){
+				swap(array, tempLowIndex, i);
+				System.out.println("becomes "+Arrays.toString(array));
+			} 
+		}//end for
+		System.out.println("Ends as "+Arrays.toString(array));
+	}//end method
+	
+	public static void bubbleSort(int[] array){
+		System.out.println("Bubble sort with "+Arrays.toString(array));
+		for(int i=0; i < array.length-1; i++){
+			for(int j=1; j < array.length-i; j++){
+				if(array[j-1] > array[j]){
+					swap(array,j-1,j);
+					System.out.println("becomes "+Arrays.toString(array));
+				}
+			}
+			System.out.println("Ends as "+Arrays.toString(array));
+		}
 	}
 
 }
