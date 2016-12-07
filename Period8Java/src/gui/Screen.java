1c193bb335d67ac867ad3eea3e658518956330e4
2MSBHVE <max@miku.nyc>
2016-12-07 14:03:12 -0500

    GUIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII

---
+++ Period8Java/src/gui/Screen.java
@@ -0,0 +1,31 @@
+package gui;
+
+import java.awt.image.*;
+
+public class Screen {
+
+	private int width;
+	private int height;
+	protected BufferedImage image;
+	
+	public Screen(int width, int height) {
+		this.width = width;
+		this.height = height;
+		initImage();
+	}
+	
+	private void initImage() {
+		image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+		update();
+	}
+
+	private void update() {
+		// this is where you draw stuff
+		
+	}
+	
+	public BufferedImage getImage() {
+		return image;
+	}
+
+}
